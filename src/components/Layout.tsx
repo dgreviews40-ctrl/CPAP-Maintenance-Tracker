a97c600166801f3c8aa3be84c485c6322b68ea51
[dyad] <git@dyad.sh>
2025-10-16 14:44:55 -0700

    [dyad] Fixing visual updates and color scheme - wrote 1 file(s)

---
+++ src/components/Layout.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { PropsWithChildren } from "react";
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+const Layout = ({ children }: PropsWithChildren) => {
+  return (
+    <TooltipProvider>
+      <main className="min-h-screen bg-background font-sans antialiased">
+        {children}
+      </main>
+      <Toaster />
+    </TooltipProvider>
+  );
+};
+
+export default Layout;
