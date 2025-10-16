6c5ce3e8ef0ea59184e0aba61300be9bf101ef5f
[dyad] <git@dyad.sh>
2025-10-16 14:14:20 -0700

    Updated src/vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_URL: https://ixzksyogrrzqzsqbsrtw.supabase.co;
   readonly VITE_SUPABASE_ANON_KEY: string;
 }
 
