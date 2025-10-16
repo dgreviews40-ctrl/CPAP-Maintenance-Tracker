c9876647bdb2cc3b1a6f22841702bbeb8e1f7bc8
[dyad] <git@dyad.sh>
2025-10-16 14:15:06 -0700

    [dyad] Testing the application - wrote 1 file(s)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_SUPABASE_URL: https://ixzksyogrrzqzsqbsrtw.supabase.co;
-  readonly VITE_SUPABASE_ANON_KEY: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4emtzeW9ncnJ6cXpzcWJzcnR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2Mjc0NDksImV4cCI6MjA3NjIwMzQ0OX0.mqNrdcwgg8nMpgDLJjqDGOWL_vO4SngZ7IIs8LbOhls;
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
 }
 
 interface ImportMeta {
